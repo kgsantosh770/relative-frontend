9142729f663eeffe1c1c19614598c168290beeb8
santosh <santhoshkrishna770@gmail.com>
2022-10-09 09:44:04 +0530

    title and ui changes

--- src/components/TrendingAssets.tsx
+++ src/components/TrendingAssets.tsx
@@ -30,9 +30,9 @@ function TrendingAssets() {
     )
 
     const assets = TrendingAssetsData.map(asset => (
-        <div 
-        key={asset.id}
-        className="trending-asset flex flex-col justify-center items-center mr-[70px] mb-9">
+        <div
+            key={asset.id}
+            className="trending-asset flex flex-col justify-center items-center mr-[70px] mb-9">
             <div
                 className="
                         asset-img 
@@ -50,12 +50,12 @@ function TrendingAssets() {
                     className="w-[49px] h-[49px]"
                 />
             </div>
-            <div className="card min-w-[291px] mt-[-50px] px-[24px] text-[12px] leading-[31px] text-center">
+            <div className="card min-w-[291px] mt-[-50px] pb-[11px] px-[24px] text-[12px] leading-[31px] text-center">
                 <p className="title text-blue-text font-bold mt-[71px]">{asset.name}</p>
                 <div className="value-status bg-primary-color rounded-2xl mt-[12.5px] px-[20px] relative flex flex-row">
-                    <span className="value text-[16px] flex-auto text-right">${asset.price.toLocaleString(undefined,{maximumFractionDigits: 20,minimumFractionDigits: 2})}</span>
-                    <span 
-                    className={`
+                    <span className="value text-[16px] flex-auto text-right">${asset.price.toLocaleString(undefined, { maximumFractionDigits: 20, minimumFractionDigits: 2 })}</span>
+                    <span
+                        className={`
                     percent-change 
                     ${asset.percentageChange < 0 ? "text-decrease-value" : "text-increase-value"}
                     w-[62px] 
@@ -64,7 +64,7 @@ function TrendingAssets() {
                 </div>
                 <p className="text-light-blue-text mt-[4.5px]">Price</p>
                 <div className="tvl bg-primary-color mx-auto rounded-full mt-[8.5px]">
-                    <p className="tvl-value text-[16px]">${asset.tvl.toLocaleString(undefined, {maximumFractionDigits: 3})}</p>
+                    <p className="tvl-value text-[16px]">${asset.tvl.toLocaleString(undefined, { maximumFractionDigits: 3 })}</p>
                 </div>
                 <p className="text-light-blue-text mt-[4.5px]">TVL</p>
                 <div className="popular-pairs flex flex-row justify-between max-w-[121px] px-[15px] py-[9px] bg-primary-color mx-auto rounded-full mt-[12px]">
@@ -78,8 +78,12 @@ function TrendingAssets() {
     ))
 
     return (
-        <section className="trending-assets text-white-text flex flex-row flex-wrap justify-center my-9">
+        <section className="trending-assets text-white-text flex flex-row flex-wrap justify-between my-9 mx-[87px]">
             {assestsVector}
+            <div className="trending-title text-[16px] leading-[32px] flex flex-row items-center w-full mb-[58px] tracking-[-0.01em]">
+                <img src="/images/trend-icon.png" alt="trending-assets" className="inline-block" />
+                <span className="ml-[16px]">Trending Assets</span>
+            </div>
             {assets}
         </section>
     )
